66a53ec4a5c4086268612efec43ff6c9b7769af7
Marco Rosner <marco.rosner@gmail.com>
2024-01-16 08:33:00 -0300

    React - Typescript - Change filename

--- React/src/index.js
+++ React/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './Contacts/App';
 // import { App } from './Context/App';
-// import { App } from './Typescript/toggle';
+// import { App } from './Typescript/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
