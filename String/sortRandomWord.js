ac2b57f1313f8d732115c2168622495bd940a3da
Marco Rosner <marco.rosner@gmail.com>
2024-01-16 07:37:29 -0300

    String - sortRandomWord

---
+++ String/sortRandomWord.js
@@ -0,0 +1,3 @@
+const sortRandomWord = (s) => s.split("").sort().join("")
+
+console.log(sortRandomWord('zzcsdewergs')) // cdeegrsswzz
--- String/sortStrings.js
+++ String/sortStrings.js
@@ -1,4 +1,4 @@
-const sortString = (arr) => arr.sort((a,b) => a.localeCompare(b))
+const sortStrings = (arr) => arr.sort((a,b) => a.localeCompare(b))
 
-console.log(sortString(["José", "Maria", "Marco", "Adalberto", "Zezo", "Daniel"]))
+console.log(sortStrings(["José", "Maria", "Marco", "Adalberto", "Zezo", "Daniel"]))
 //  ['Adalberto', 'Daniel', 'José', 'Marco', 'Maria', 'Zezo']
