64b317f499c17bbbde059737e4ddf1c93a870190
Marco Rosner <marco.rosner@gmail.com>
2024-01-16 16:54:25 -0300

    String - countingLetters

---
+++ String/countingLetters.js
@@ -0,0 +1,22 @@
+const countingLetters = (s) => {
+    const lowercase = s.toLowerCase()
+    const report = {}
+
+    for(let i=0; i<lowercase.length; i++) {
+        if(lowercase[i] == ' ') continue
+
+        if(report[lowercase[i]]) {
+            report[lowercase[i]] =  report[lowercase[i]] + 1
+        } else {
+            report[lowercase[i]] = 1
+        }
+    }
+
+    return report
+}
+
+console.log(countingLetters('aba'))
+console.log(countingLetters('aBa'))
+console.log(countingLetters('aabbaccddd'))
+console.log(countingLetters(''))
+console.log(countingLetters('a b a'))
