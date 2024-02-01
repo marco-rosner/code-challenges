bab5eb692f7ce78c9f3642ac018984767eeac725
Marco Rosner <marco.rosner@gmail.com>
2024-02-01 17:26:57 -0300

    React - PaginationPointer - refactor calculation

--- React/src/Pagination/PaginationPointer.js
+++ React/src/Pagination/PaginationPointer.js
@@ -4,25 +4,23 @@ export const PaginationPointer = ({ data }) => {
     const itensPerPage = 10
     const [items, setItems] = useState(data.slice(0, itensPerPage))
 
-    const getIndex = (data, move) => {
-        return move === 'next'
-            ? data.indexOf(data[data.indexOf(items[items.length-1])])
-            : data.indexOf(data[data.indexOf(items[0])])
-    }
+    const getPreviousIndex = () => data.indexOf(data[data.indexOf(items[0])])
 
     const previous = () => {
         setItems(
             data.slice(
-                getIndex(data, 'previous') - itensPerPage,
-                getIndex(data, 'previous') ,
+                getPreviousIndex() - itensPerPage,
+                getPreviousIndex() ,
             ))
     }
+
+    const getNextIndex = () => data.indexOf(data[data.indexOf(items[items.length-1])])
 
     const next = () => {
         setItems(
             data.slice(
-                getIndex(data, 'next') + 1,
-                (getIndex(data, 'next') + 1 + itensPerPage)
+                (getNextIndex() + 1),
+                (getNextIndex() + 1 + itensPerPage)
             ))
     }
 
@@ -33,8 +31,8 @@ export const PaginationPointer = ({ data }) => {
                     <li key={item}>{item}</li>
                 ))}
             </ul>
-            <button onClick={previous} disabled={getIndex(data, 'previous') <= 0}>Previous Page</button>
-            <button onClick={next} disabled={getIndex(data, 'next') >= data.length}>Next Page</button>
+            <button onClick={previous} disabled={getPreviousIndex() <= 0}>Previous Page</button>
+            <button onClick={next} disabled={getNextIndex() >= data.length}>Next Page</button>
         </>
     )
 }
