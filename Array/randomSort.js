cd4f75e7170a7fd22786eba87ca445ffa21dbcca
Marco Rosner <marco.rosner@gmail.com>
2024-02-23 09:11:33 -0300

    Array - randomSort

---
+++ Array/randomSort.js
@@ -0,0 +1,8 @@
+const randomSort = (arr) => {
+    return arr.sort(() => 0.5 - Math.random())
+}
+
+console.log(randomSort([9, -1, 5, 2, -3, 5]))
+console.log(randomSort([9, -1, 5, 2, -3, 5]))
+console.log(randomSort([9, -1, 5, 2, -3, 5]))
+console.log(randomSort([9, -1, 5, 2, -3, 5]))
