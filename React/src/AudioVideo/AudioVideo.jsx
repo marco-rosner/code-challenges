2caccf6c8b9498759b8207273e78d22ea6c323fe
Marco Rosner <marco.rosner@gmail.com>
2024-02-28 21:33:59 -0300

    React - AudioVideo

---
+++ React/src/AudioVideo/AudioVideo.jsx
@@ -0,0 +1,67 @@
+import { Box, Button, Grid } from "@mui/material"
+
+export const Video = () => {
+    return (
+        <video
+            id="video"
+            controls
+            loop
+            preload="auto"
+            width={320}
+            height={320}
+        >
+            <source src="rabbit320.mp4" type="video/mp4" />
+            <p>No supported</p>
+        </video>
+    )
+}
+
+export const Audio = () => {
+    return (
+        <audio
+            id="audio"
+            controls
+        >
+            <source src="rabbit320.mp4" type="video/mp4" />
+            <p>No supported</p>
+        </audio>
+    )
+}
+
+export const App = () => {
+
+    const onPlay = () => {
+        const audio = document.getElementById("audio")
+        const video = document.getElementById("video")
+
+        audio.play()
+        video.play()
+    }
+
+    const onStop = () => {
+        const audio = document.getElementById("audio")
+        const video = document.getElementById("video")
+
+        audio.pause()
+        video.pause()
+    }
+
+
+    return (
+        <Grid
+            container
+            direction="column"
+            justifyContent="space-evenly"
+            alignContent="center"
+            alignItems="center"
+            sx={{ minHeight: '100vh' }}
+        >
+            <Video />
+            <Audio />
+            <Box>
+                <Button onClick={onPlay}>Play</Button>
+                <Button onClick={onStop}>Stop</Button>
+            </Box>
+        </Grid>
+    )
+}
--- React/src/index.js
+++ React/src/index.js
@@ -2,13 +2,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from './AudioVideo/AudioVideo';
 // import { App } from './SearchUsers/App';
 // import { App } from './InfiniteGallery/App';
 // import { App } from './Contacts/App';
 // import { App } from './Context/App';
 // import { App } from './Pagination/App';
 // import { App } from './Typescript/App';
-import { App } from './ToDo/App';
+// import { App } from './ToDo/App';
 import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
