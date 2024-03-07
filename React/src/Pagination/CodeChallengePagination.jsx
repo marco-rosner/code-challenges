9097fb063cd940ece6b21ec1055d30f854c4ec5a
Marco Rosner <marco.rosner@gmail.com>
2024-03-07 20:34:09 -0300

    React - Pagination- Code Challenge

---
+++ React/src/Pagination/CodeChallengePagination.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+
+const USERS_URL = 'https://example.com/api/users';
+const PER_PAGE = 10
+const INITIAL_PAGE = 0
+
+export default function Table() {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [users, setUsers] = useState([])
+    const [page, setPage] = useState(INITIAL_PAGE)
+    const [lastPage, setLastPage] = useState(0)
+
+    const fetchUsers = (page) => {
+        setLoading(true)
+        fetch(`${USERS_URL}?page=${page}`)
+            .then((data) => {
+                setLoading(false)
+                return data.json()
+            })
+            .then((data) => {
+                setLastPage(Math.round(data.count / PER_PAGE))
+                setUsers(data.results)
+            })
+            .catch((err) => {
+                console.log(err)
+                setError(!!err)
+            })
+    }
+
+    useEffect(() => {
+        fetchUsers(page)
+    }, [])
+
+    useEffect(() => {
+        if (!loading) {
+            fetchUsers(page)
+        }
+    }, [page])
+
+    if (error) return <>Something went wrong</>
+
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user) => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.firstName}</td>
+                            <td>{user.lastName}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <section className="pagination">
+                <button className="first-page-btn" disabled={loading || page === INITIAL_PAGE} onClick={() => setPage(INITIAL_PAGE)}>first</button>
+                <button className="previous-page-btn" disabled={loading || page === INITIAL_PAGE} onClick={() => setPage(page - 1)}>previous</button>
+                <button className="next-page-btn" disabled={loading || page === lastPage} onClick={() => setPage(page + 1)}>next</button>
+                <button className="last-page-btn" disabled={loading || page === lastPage} onClick={() => setPage(lastPage)}>last</button>
+            </section>
+        </div>
+    );
+};
