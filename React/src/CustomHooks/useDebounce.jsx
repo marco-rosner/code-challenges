046996c128d15f7ae062484324cf9a88e590d389
Marco Rosner <marco.rosner@gmail.com>
2024-03-18 15:00:31 -0300

    React - Custom Hooks - simplify code

--- React/src/CustomHooks/useDebounce.jsx
+++ React/src/CustomHooks/useDebounce.jsx
@@ -1,13 +1,12 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 
 export const useDebounce = (value, delay = 500) => {
     const [debouncedValue, setDebouncedValue] = useState('')
-    const timeRef = useRef()
 
     useEffect(() => {
-        timeRef.current = setTimeout(() => setDebouncedValue(value), delay)
+        const timeout = setTimeout(() => setDebouncedValue(value), delay)
 
-        return () => clearTimeout(timeRef.current)
+        return () => clearTimeout(timeout)
     }, [value, delay])
 
     return debouncedValue
