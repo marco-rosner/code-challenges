88fb60a954808fe77f18e151bc8325f2f5d23bca
Marco Rosner <marco.rosner@gmail.com>
2024-03-27 10:43:10 -0300

    DS - DoubleLinkedList

---
+++ Data Structure/DoubleLinkedList.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(key, value, prev = null, next = null) {
+        this.key = key
+        this.value = value
+        this.prev = prev
+        this.next = next
+    }
+}
+
+class DoubleLinkedList {
+    constructor() {
+        this.head = new Node(0, 0)
+        this.tail = new Node(0, 0)
+        this.head.next = this.tail
+        this.tail.prev = this.head
+    }
+
+    insertHead(node) {
+        node.next = this.head.next
+        node.prev = this.head
+        this.head.next.prev = node
+        this.head.next = node
+    }
+
+    remove(node) {
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    }
+
+    removeLast() {
+        let node = this.tail.prev
+        let key = node.key
+
+        this.remove(node)
+
+        return key
+    }
+}
