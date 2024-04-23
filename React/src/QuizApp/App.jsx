94097965840b55a5566411aea4a85f42307e73e5
Marco Rosner <marco.rosner@gmail.com>
2024-04-23 11:49:13 -0300

    React - QuizApp - Applying Flexible Box Layout

--- React/src/QuizApp/App.jsx
+++ React/src/QuizApp/App.jsx
@@ -1,15 +1,9 @@
+import { Grid } from '@mui/material';
 import React, { useState } from 'react';
 import { Question } from './Question';
 import { Result } from './Result';
 
 const style = {
-    container: {
-        padding: '20px',
-        border: '1px solid #E0E0E0',
-        borderRadius: '15px',
-        width: 'max-content',
-        marginBottom: '40px',
-    },
     feedback: {
         marginTop: '10px',
         fontSize: '14px',
@@ -33,14 +27,22 @@ export const App = () => {
     const [score, setScore] = useState([])
 
     return (
-        <div style={style.container}>
-            {score.length !== questions.length ?
-                (<>
-                    <Question questions={questions} setScore={setScore} />
-                    <div id="feedback" style={style.feedback}>{score[score.length - 1]}</div>
-                </>) :
-                (<Result score={score} questionTotal={questions.length} />)
-            }
-        </div >
+        <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ minHeight: '100vh' }}>
+            <Grid
+                sx={{ border: '1px solid #E0E0E0', borderRadius: '15px', padding: '20px' }}>
+                {score.length !== questions.length ?
+                    (<>
+                        <Question questions={questions} setScore={setScore} />
+                        <div id="feedback" style={style.feedback}>{score[score.length - 1]}</div>
+                    </>) :
+                    (<Result score={score} questionTotal={questions.length} />)
+                }
+            </Grid>
+        </Grid>
     );
 }
