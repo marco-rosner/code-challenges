1b845a714a7471bee4eda1c0d83ac14d3fe3c838
Marco Rosner <marco.rosner@gmail.com>
2024-04-30 09:21:10 -0300

    Array - candles - performance test using reduce

--- Array/candles.js
+++ Array/candles.js
@@ -10,11 +10,28 @@ function birthdayCakeCandles(candles) {
     const max = Math.max(...candles)
     let numMax = 0
 
-    for(let i=0;i<candles.length;i++){
-        if(max == candles[i]) numMax++
+    const start = performance.now()
+    for (let i = 0; i < candles.length; i++) {
+        if (max == candles[i]) numMax++
     }
+    const end = performance.now()
+
+    console.log('for >', numMax, end - start)
 
     return numMax
 }
 
-console.log(birthdayCakeCandles([3,2,1,3]))
+const birthdayCakeCandlesReduce = (candles) => {
+    const max = Math.max(...candles)
+    let numMax
+
+    const start1 = performance.now()
+    numMax = candles.reduce((acc, cur) => max == cur ? acc + 1 : acc, 0)
+    const end1 = performance.now()
+
+    console.log('reduce >', numMax, end1 - start1)
+    return numMax
+}
+
+console.log(birthdayCakeCandles([3, 2, 1, 3]))
+console.log(birthdayCakeCandlesReduce([3, 2, 1, 3]))
