27ffff96f00f49e7f0d9bef06ccac26d30ff50e5
Marco Rosner <marco.rosner@gmail.com>
2024-06-18 20:49:22 -0300

    React - Code Challenge - Add AbortController

--- React/src/CodeChallenge/useFetch.jsx
+++ React/src/CodeChallenge/useFetch.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 
 export const useFetch = (url) => {
+    const abortController = AbortController()
     const [isLoading, setIsLoading] = useState(false)
     const [isError, setIsError] = useState(false)
     const [data, setData] = useState([])
@@ -8,15 +9,23 @@ export const useFetch = (url) => {
     useEffect(() => {
         setIsLoading(true)
 
-        fetch(url)
+        const fetchFunciton = async (url) => await fetch(url, { signal: abortController })
             .then((data) => data.json())
             .then(setData)
             .catch((err) => {
-                console.error(err)
+                if (err.name === 'AbortError') {
+                    console.error('Aborted due Race Condition')
+                } else {
+                    console.error(err)
+                }
+
                 setIsError(!!err)
             })
             .finally(() => setIsLoading(false))
 
+        fetchFunciton(url)
+
+        return () => abortController.abort()
     }, [url])
 
     return { data, isLoading, isError }
